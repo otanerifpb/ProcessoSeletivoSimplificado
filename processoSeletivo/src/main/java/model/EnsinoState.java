fcd0ee87a7f11729f0ffb614e767c464ede647f2
otanerifpb <renato.ribeiro.pro@gmail.com>
2023-06-22 22:24:43 -0300

    Update EnsinoState.java
    Atualização, adição do State Experiência de Ensino

--- processoSeletivo/src/main/java/model/EnsinoState.java
+++ processoSeletivo/src/main/java/model/EnsinoState.java
@@ -5,8 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class EnsinoState extends State {
+	static int resposta;
 
-	EnsinoState(Candidato candidato) {
+	public EnsinoState(Candidato candidato) {
 		super(candidato);
 		// TODO Auto-generated constructor stub
 	}
@@ -14,49 +15,50 @@ public class EnsinoState extends State {
 	@Override
 	public String clasificacao() throws NumberFormatException, IOException {
 		if (candidato.isProcess()) {
-			if (ensinoCandidato() == 1) {
-				candidato.setStatus("Classificado");
+			resposta = ensinoCandidato();
+			while (resposta != 0 && resposta != 1 && resposta != 2 && resposta != 3) {
+				resposta = ensinoCandidato();
+			}
+			
+			int ensino = resposta;
+			if (ensino == 1) {
+				//candidato.setStatus("Classificado");
 				candidato.setNota(candidato.getNota() + 12);
 				candidato.changeState(new GestaoState(candidato));
-				return "        Candidato Classificado" + 
-						"Candidato elegível para Prova de Ensino";
-			} else if (ensinoCandidato() == 2){
-				candidato.setStatus("Classificado");
+				return "     Candidato elegível para Prova de Gestão"; 	
+			} else if (ensino == 2){
+				//candidato.setStatus("Classificado");
 				candidato.setNota(candidato.getNota() + 7);
 				candidato.changeState(new GestaoState(candidato));
-				return "        Candidato Classificado" + 
-						"Candidato elegível para Prova de Ensino";
-			}else if (ensinoCandidato() == 3){
-				candidato.setStatus("Classificado");
+				return "     Candidato elegível para Prova de Gestão"; 	
+			}else if (ensino == 3){
+				//candidato.setStatus("Classificado");
 				candidato.setNota(candidato.getNota() + 6);
 				candidato.changeState(new GestaoState(candidato));
-				return "        Candidato Classificado" + 
-						"Candidato elegível para Prova de Ensino";
+				return "     Candidato elegível para Prova de Gestão";
 			}else {
-				candidato.setStatus("Classificado");
+				//candidato.setStatus("Classificado");
 				//candidato.setNota(candidato.getNota() + 0);
 				candidato.changeState(new GestaoState(candidato));
-				return "        Candidato Classificado" + 
-						"Candidato elegível para Prova de Ensino";	
+				return "     Candidato elegível para Prova de Gestão";			
 			}
 		} else {
 			candidato.changeState(new TituloState(candidato));
-			return "Experiência de Ensino do Candidato em análise";
+			return "     Experiência de Ensino do Candidato em análise";
 		}
 	}
 	
 	public static int ensinoCandidato() throws NumberFormatException, IOException  {
-		int resposta = 0;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		if (resposta != 0 && resposta != 1 && resposta != 2 && resposta != 3) {
-			System.out.println("            Resposta Inválida!!");
-			System.out.println("Informe um Valor Dentro do Intervalo Indicado!");
-		} else {
-			System.out.println("Informar o Experiência de Ensino do Candidato");
-		}
-		System.out.println("Informe: 0 - NENHUM; 1 - Intituto Federal; "
-				+ "2 - Educação Superior; 3 - Ensino Médio");
+		System.out.println("     Informe: NENHUM (0); Intituto Federal (1); "
+				+ "Educação Superior (2); Ensino Médio (3)");
 		resposta = Integer.parseInt(br.readLine());
+		if (resposta != 0 && resposta != 1 && resposta != 2 && resposta != 3) {
+			System.out.println("     Resposta Inválida!!");
+			System.out.println("     Informe um Valor Dentro do Intervalo Indicado!");
+		} else {
+			System.out.println("     Informar a Experiência de Ensino do Candidato");
+		}
 		return resposta;
 	}
 
