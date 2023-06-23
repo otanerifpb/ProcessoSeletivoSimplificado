1b0ac0d0f2142eca944a0a6acd3a5aaec82e93e7
otanerifpb <renato.ribeiro.pro@gmail.com>
2023-06-22 22:59:59 -0300

    Update GestaoState.java
    Adição do State Experiência em Gestão Acadêmica

--- processoSeletivo/src/main/java/model/GestaoState.java
+++ processoSeletivo/src/main/java/model/GestaoState.java
@@ -5,50 +5,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class GestaoState extends State {
+	static int resposta;
 
-	GestaoState(Candidato candidato) {
+	public GestaoState(Candidato candidato) {
 		super(candidato);
 	}
 
 	@Override
 	public String clasificacao() throws NumberFormatException, IOException {
 		if (candidato.isProcess()) {
-			if (gestaoCandidato() == 1) {
-				candidato.setStatus("Classificado");
+			resposta = gestaoCandidato();
+			while (resposta != 0 && resposta != 1 && resposta != 2) {
+				resposta = gestaoCandidato();
+			}
+			
+			int gestao = resposta;
+			if (gestao == 1) {
+				//candidato.setStatus("Classificado");
 				candidato.setNota(candidato.getNota() + 15);
 				candidato.changeState(new FinalizadoState(candidato));
-				return "        Candidato Classificado" + 
-						"Candidato elegível para o Processo Seletivo";
-			} else if (gestaoCandidato() == 2){
-				candidato.setStatus("Classificado");
+				return "     Candidato elegível para o Processo Seletivo";	
+			} else if (gestao == 2){
+				//candidato.setStatus("Classificado");
 				candidato.setNota(candidato.getNota() + 10);
 				candidato.changeState(new FinalizadoState(candidato));
-				return "        Candidato Classificado" + 
-						"Candidato elegível para o Processo Seletivo";
+				return "     Candidato elegível para o Processo Seletivo"; 	
 			}else {
-				candidato.setStatus("Classificado");
+				//candidato.setStatus("Não Classificado");
 				//candidato.setNota(candidato.getNota() + 0);
 				candidato.changeState(new FinalizadoState(candidato));
-				return "        Candidato Classificado" + 
-						"Candidato elegível para o Processo Seletivo";	
+				return "     Candidato elegível para o Processo Seletivo";		
 			}
 		} else {
 			candidato.changeState(new EnsinoState(candidato));
-			return "Experiência de Gestão Acadêmica do Candidato em análise";
+			return "     Experiência de Gestão Acadêmica do Candidato em análise";
 		}
 	}
 	
 	public static int gestaoCandidato() throws NumberFormatException, IOException  {
-		int resposta = 0;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		if (resposta != 0 && resposta != 1 && resposta != 2) {
-			System.out.println("           Resposta Inválida!!");
-			System.out.println("Informe um Valor Dentro do Intervalo Indicado!");
-		} else {
-			System.out.println("Informar o Experiência de Ensino do Candidato");
-		}
-		System.out.println("Informe: 0 - NENHUM; 1 - Cargo de Diretor; 2 - Coordenador de Curso");
+		System.out.println("Informe: NENHUM (0); Cargo de Diretor (1); Coordenador de Curso (2)");
 		resposta = Integer.parseInt(br.readLine());
+		if (resposta != 0 && resposta != 1 && resposta != 2) {
+			System.out.println("     Resposta Inválida!!");
+			System.out.println("     Informe um Valor Dentro do Intervalo Indicado!");
+		} else {
+			System.out.println("     Informar o Experiência de Ensino do Candidato");
+		}
+		
 		return resposta;
 	}
 
