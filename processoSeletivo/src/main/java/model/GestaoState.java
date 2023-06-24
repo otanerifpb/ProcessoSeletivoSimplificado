40f32100e4d0d4b258d0311bced5f993558e5a4e
otanerifpb <renato.ribeiro.pro@gmail.com>
2023-06-24 18:34:51 -0300

    Update GestaoState.java
    Atualização
    Adição do Set e HashSet para guardar as respostas que o Candidato escolhe na opção para não deixar digitar mais de uma vez a mesma opção pelo controle com o uso do while, e a notaCandidato() foi adicionada dentro de um método.

--- processoSeletivo/src/main/java/model/GestaoState.java
+++ processoSeletivo/src/main/java/model/GestaoState.java
@@ -3,9 +3,12 @@ package model;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GestaoState extends State {
 	static int resposta;
+	Set<Integer> respostas = new HashSet<Integer>();
 
 	public GestaoState(Candidato candidato) {
 		super(candidato);
@@ -15,27 +18,18 @@ public class GestaoState extends State {
 	public String clasificacao() throws NumberFormatException, IOException {
 		if (candidato.isProcess()) {
 			resposta = gestaoCandidato();
-			while (resposta != 0 && resposta != 1 && resposta != 2) {
+			
+			while (resposta != 0) {
+				if (respostas.contains(resposta)) {
+					System.out.println("     Esta resposta já foi Informada, informe outra resposta!!");
+					resposta = gestaoCandidato();
+				}
 				resposta = gestaoCandidato();
+				notaCandidato(candidato);
 			}
 			
-			int gestao = resposta;
-			if (gestao == 1) {
-				//candidato.setStatus("Aprovado");
-				candidato.setNota(candidato.getNota() + 15);
-				candidato.changeState(new FinalizadoState(candidato));
-				return "     Candidato elegível para o Processo Seletivo";	
-			} else if (gestao == 2){
-				//candidato.setStatus("Aprovado");
-				candidato.setNota(candidato.getNota() + 10);
-				candidato.changeState(new FinalizadoState(candidato));
-				return "     Candidato elegível para o Processo Seletivo"; 	
-			}else {
-				//candidato.setStatus("Eliminado");
-				//candidato.setNota(candidato.getNota() + 0);
-				candidato.changeState(new FinalizadoState(candidato));
-				return "     Candidato elegível para o Processo Seletivo";		
-			}
+			candidato.changeState(new FinalizadoState(candidato));
+			return "     Candidato elegível para o Processo Seletivo";
 		} else {
 			candidato.changeState(new EnsinoState(candidato));
 			return "     Experiência de Gestão Acadêmica do Candidato em análise";
@@ -44,16 +38,31 @@ public class GestaoState extends State {
 	
 	public static int gestaoCandidato() throws NumberFormatException, IOException  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Informe: NENHUM (0); Cargo de Diretor (1); Coordenador de Curso (2)");
+		System.out.println("     Informar a Experiência de Gestão do Candidato");
+		System.out.println("     Informe: Sair Experiência Gestão(0); Cargo de Diretor(1); "
+				+ "Coordenador de Curso(2)");
 		resposta = Integer.parseInt(br.readLine());
 		if (resposta != 0 && resposta != 1 && resposta != 2) {
 			System.out.println("     Resposta Inválida!!");
 			System.out.println("     Informe um Valor Dentro do Intervalo Indicado!");
-		} else {
-			System.out.println("     Informar o Experiência de Ensino do Candidato");
-		}
-		
+		} 
 		return resposta;
 	}
-
+	
+	public void notaCandidato(Candidato candidato) {
+		int gestao = resposta;
+		respostas.add(gestao);
+		//System.out.println(respostas);
+		if (gestao == 1) {
+			//candidato.setStatus("Aprovado");
+			candidato.setNota(candidato.getNota() + 15); 	
+		} else if (gestao == 2){
+			//candidato.setStatus("Aprovado");
+			candidato.setNota(candidato.getNota() + 10);	
+		} else {
+			//candidato.setStatus("Eliminado");
+			//candidato.setNota(candidato.getNota() + 0);
+			candidato.changeState(new FinalizadoState(candidato));			
+		}
+	}
 }
